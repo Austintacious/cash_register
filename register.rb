ae91a9322da2278456375da5e26c5dded3e95374
Austintacious <awinslow21@gmail.com>
2013-11-15 13:39:56 -0500

    Update register.rb
    Needs decimals fixed

--- register.rb
+++ register.rb
@@ -1,52 +1,42 @@
-public
-def validate_type?(input)
-  if (input.is_a?(Fixnum) ^ input.is_a?(Float)) != true
-    puts "Wrong type of input"
-  else
-    return input.to_f
+class Register
+
+  def validate_input(input)
+    input =~ /\A\$?\d+(.\d{1,2})?\z/
   end
-end
 
-public
-def validate_decimal?(input)
-  if !(input.to_s =~ (/\A\$?\d+(.\d{1,2})?\z/))
-    puts "Wrong type of input"
-  else
-    return input
+  def get_the_price
+    puts "What is the price? (Enter a valid dollar amount)"
+    @price = gets.chomp
+    (validate_input(@price)) ? get_the_tendered : get_the_price
   end
-end
 
-public
-def get_the_price
-  puts "What is the price?"
-  $price = gets.chomp
-end
+  def get_the_tendered
+    puts "What is the tendered amount?"
+    @tendered = gets.chomp
+    (validate_input(@tendered)) ? validate_amount(@price, @tendered) : get_the_tendered
+  end
 
-public
-def get_the_tendered
-  puts "What is the tendered amount?"
-  $tendered = gets.chomp
-end
-
-public
-def validate_amount(price, tendered)
-  if tendered < price
-    puts "Not enough! You still owe #{(amount - given).round(2)}"
-  else
-    puts "===Thank You!===
-    The total change due is $ #{given - amount}
+  def calculate(tendered, price)
+    puts "
+    ===Thank You!===
+    The total change due is $ #{tendered.to_f - price.to_f}
     #{Time.now}
     ================"
   end
-end
 
-def check_input
-  if get_the_price.validate_type?($price).validate_decimal?($price)
-    get_the_tendered.validate_type($tendered).validate_decimal($tendered).validate_amount($price, $tendered)
-  else
-    check_input
+  def validate_amount(price, tendered)
+    if @tendered.to_f < @price.to_f
+      puts "Not enough! You still owe #{sprintf('$%0.2f', price.to_f - tendered.to_f)}" 
+      get_the_tendered
+    else 
+      calculate(@tendered, @price)
+    end
   end
+
 end
 
-
+x = Register.new
+x.get_the_price
+x.validate_input
+x.validate_amount
 check_input
